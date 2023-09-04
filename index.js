baf80c8a1b6b238c9340538dd725ce3b871c82a3
Garrett <lamgarr@outlook.com>
2023-09-03 21:17:21 -0700

    Connect db

--- index.js
+++ index.js
@@ -9,8 +9,15 @@ const db = mysql.createConnection({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.evnv.MYSQL_DATABASE
+    database: process.env.MYSQL_DATABASE
 });
+
+db.connect((err) => {
+    if(err) {
+        throw err;
+    }
+    console.log("Connected");
+})
 
 const app = express();
 
