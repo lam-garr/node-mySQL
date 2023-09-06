f2fdea8cced50195a7bcc942cb5cbf75fbe80f3b
Garrett <lamgarr@outlook.com>
2023-09-05 21:32:26 -0700

    Add query for creating table

--- index.js
+++ index.js
@@ -29,6 +29,14 @@ app.get("/createdb", (req, res) => {
     })
 });
 
+app.get("createtable", (req, res) => {
+    let sql = "CREATE TABLE user(id int AUTO_INCREMENT, name VARCHAR(255), desc VARCHAR(225), PRIMARY KEY (id))";
+    db.query(sql, (err, result) => {
+        if(err) throw err;
+        res.send("Successfully created table");
+    });
+});
+
 app.listen(process.env.PORT, ()=> {
     console.log(`Server listening on port ${process.env.PORT}`);
 })
